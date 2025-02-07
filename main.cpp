2da065f9f4f31c80100bc7faad6aeec5c8f69d16
オジマアキト <k023g0097@g.neec.ac.jp>
2025-02-07 12:53:40 +0900

    05_01完了

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <Novice.h>
-#include "GameManager.h"
+#include "Scene.h"
 
-const char kWindowTitle[] = "LE2*_**_***_***";
+const char kWindowTitle[] = "LE2D_05_オジマ_アキト";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -9,17 +9,40 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// 生成
-	GameManager* gameManager = new GameManager();
+	Scene* scene = new Scene();
+	scene->Initialize();
 
-	// ゲーム実行
-	gameManager->Run();
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
 
-	// 解放
-	delete gameManager;
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		/// ↓更新処理ここから
+		scene->Update();
+		/// ↑更新処理ここまで
+
+		/// ↓描画処理ここから
+		scene->Draw();
+		/// ↑描画処理ここまで
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
 
 	// ライブラリの終了
 	Novice::Finalize();
-
 	return 0;
 }
